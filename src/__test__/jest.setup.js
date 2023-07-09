aa663d34db828511e65b14be2d8cf5a221dfdc2c
Rui <rui19981120.sns@gmail.com>
2023-07-09 18:05:52 +0900

    Bug
    Define a mock so that 'window.matchMedia' can be used in the just test environment

--- src/__test__/jest.setup.js
+++ src/__test__/jest.setup.js
@@ -2,6 +2,14 @@ import { render } from '@testing-library/react';
 import ReduxProviders from '@/components/ReduxProvider';
 import ChakraProviders from '@/components/ChakraProvider';
 
+Object.defineProperty(window, 'matchMedia', {
+	value: () => ({
+		matches: false,
+		addListener: () => {},
+		removeListener: () => {},
+	}),
+});
+
 const customRender = (ui, options) =>
 	render(
 		<ReduxProviders>
