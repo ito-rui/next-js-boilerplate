74ee807c1d2bbfd5a4d777e1fb58e0ae716e8525
Rui <rui19981120.sns@gmail.com>
2023-07-09 17:48:09 +0900

    Modify
    Add each theme

--- src/components/theme/index.ts
+++ src/components/theme/index.ts
@@ -1,6 +1,20 @@
 import { extendTheme } from '@chakra-ui/react';
+import config from './config';
+import breakpoints from './breakpoint';
 import colors from './colors';
+import components from './components';
 
-const theme = extendTheme({ colors });
+const theme = extendTheme({
+	colors,
+	config,
+	components,
+	breakpoints,
+	styles: {
+		global: {
+			body: {},
+			div: {},
+		},
+	},
+});
 
 export default theme;
